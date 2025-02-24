e005b6210fc7d207593badded5a61503f87d42c4
Billal Saidi <63353891+billal280@users.noreply.github.com>
2025-02-24 17:33:09 +0100

    Updated terraform.groovy

--- src/org/billal/terraform.groovy
+++ src/org/billal/terraform.groovy
@@ -3,18 +3,54 @@ package org.billal
 class Terraform {
 
     def terraformPlan(String environment) {
-        withCredentials([usernamePassword(credentialsId: 'AWS_ACCESS_KEY', usernameVariable: '', passwordVariable: '')]) {
-            sh """
-                terraform -chdir=environment/${environment} plan -no-color -out=tfplan
-            """
+        withEnv([
+            "SELECTED_ENV=${selectedEnv}"
+        ]) {
+            withCredentials([
+                usernamePassword(
+                    credentialsId: "${selectedEnv.toUpperCase()}_TERRAFORM_BACKEND_S3_CREDENTIALS",
+                    usernameVariable: 'AWS_ACCESS_KEY',
+                    passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+                ),
+                usernamePassword(
+                    credentialsId: "${selectedEnv.toUpperCase()}_TERRAFORM_BACKUP_S3_CREDENTIALS",
+                    usernameVariable: "TF_VAR_${selectedEnv}_s3_backup_access_key",
+                    passwordVariable: "TF_VAR_${selectedEnv}_s3_backup_secret_key"
+                ),
+                usernamePassword(
+                    credentialsId: "${selectedEnv.toUpperCase()}_TERRAFORM_OPENSEARCH_ADMIN_CREDENTIALS",
+                    usernameVariable: "TF_VAR_${selectedEnv}_admin_opensearch_username",
+                    passwordVariable: "TF_VAR_${selectedEnv}_admin_opensearch_password"
+                )
+            ]) {
+                sh 'terraform -chdir="environments/${SELECTED_ENV}" plan -no-color'
+            }
         }
     }
 
     def terraformApply(String environment) {
-        withCredentials([usernamePassword(credentialsId: 'AWS_ACCESS_KEY', usernameVariable: '', passwordVariable: '')]) {
-            sh """
-                terraform -chdir=environment/${environment} apply -auto-approve tfplan
-            """
+        withEnv([
+            "SELECTED_ENV=${selectedEnv}"
+        ]) {
+            withCredentials([
+                usernamePassword(
+                    credentialsId: "${selectedEnv.toUpperCase()}_TERRAFORM_BACKEND_S3_CREDENTIALS",
+                    usernameVariable: 'AWS_ACCESS_KEY',
+                    passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+                ),
+                usernamePassword(
+                    credentialsId: "${selectedEnv.toUpperCase()}_TERRAFORM_BACKUP_S3_CREDENTIALS",
+                    usernameVariable: "TF_VAR_${selectedEnv}_s3_backup_access_key",
+                    passwordVariable: "TF_VAR_${selectedEnv}_s3_backup_secret_key"
+                ),
+                usernamePassword(
+                    credentialsId: "${selectedEnv.toUpperCase()}_TERRAFORM_OPENSEARCH_ADMIN_CREDENTIALS",
+                    usernameVariable: "TF_VAR_${selectedEnv}_admin_opensearch_username",
+                    passwordVariable: "TF_VAR_${selectedEnv}_admin_opensearch_password"
+                )
+            ]) {
+                sh 'terraform -chdir="environments/${SELECTED_ENV}" apply --auto-approve -no-color'
+            }
         }
     }
 }
