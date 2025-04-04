fbb7aa2ab4f7d2814aa9aae7bc7b9aead2fd7337
Billal Saidi <63353891+billal280@users.noreply.github.com>
2025-04-04 16:53:38 +0200

    Added environments and updated stages

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -1,17 +1,22 @@
+def environments = ['dev', 'int', 'uat', 'preprod', 'prod']
 pipeline {
     agent any
     parameters {
-        string (name 'Harbor', defaultValue: 'http://localhost:4442', description: 'Harbor registry to pull OCI')
+        string (name 'HARBOR', defaultValue: 'http://localhost:4442', description: 'Harbor registry')
+        string(name: 'HARBOR_REGISTRY_CERT_FILEPATH', defaultValue: 'certs/auth.crt', description: 'Harbor certificates file')
+        choice(name: 'ENVIRONMENT', choices: environments, description: 'Provision for the selected environment')
+        booleanParam(name: 'AUTOAPPLY', defaultValue: false, description: 'Ask for confirmation after plan before apply')
+        
     }
     stages {
-        stage ('Terraform Plan'){
+        stage ('Terraform Build'){
             steps {
-                terraform.terraformPlan()
+                terraform.terraformBuild(params.HARBOR,  params.ENVIRONMENT)
             }
         }
-        stage ('Terraform Apply'){
+        stage ('Terraform Provisionning'){
             steps {
-                terraform.terraformApply()
+                terraform.terraformApply(params.HARBOR,  params.ENVIRONMENT, params.AUTOAPPLY)
             }
         }
     }
